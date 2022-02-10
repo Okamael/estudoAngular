c9997c22398202a73e2305dd1a0e251f4279612f
Murilo Ramos da silva <okamael.adc@gmail.com>
2022-02-09 22:25:33 -0300

    Complemento estudos angular

--- bytebank/src/app/app.component.ts
+++ bytebank/src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bytebank';
+  destino:number;
+  valor:number
+  transferencia:any;
+  transferir($event){
+
+    console.log($event);
+
+    this.transferencia = $event;
+
+  }
 }
